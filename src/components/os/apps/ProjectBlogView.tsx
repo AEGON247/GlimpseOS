fbece346c09a407a59c73c8ec223f26c9cec609e
Sachin Mehta <sachinmehta247@gmail.com>
2025-11-08 21:51:28 +0530

    removed unnecessary text

--- src/components/os/apps/ProjectBlogView.tsx
+++ src/components/os/apps/ProjectBlogView.tsx
@@ -140,12 +140,6 @@ const ProjectBlogView: React.FC<ProjectBlogProps> = ({ project, onBack }) => {
         </TechStack>
 
         <BlogText>{project.description}</BlogText>
-        
-        {/* You can add more text/lorem ipsum here */}
-        <BlogText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </BlogText>
 
         <LinkContainer>
           {project.link !== '#' && (
